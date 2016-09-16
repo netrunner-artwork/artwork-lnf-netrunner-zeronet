23bdbed496a6e3799b27902f7e3810d07fc88e67
Rohan Garg <rohan@garg.io>
2016-09-16 15:37:21 +0200

    Set wallpaper to scaled and cropped

--- usr/share/plasma/look-and-feel/org.kde.netrunner-core.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-core.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -36,6 +36,7 @@ for (var j = 0; j < desktopsArray.length; j++) {
     desktopsArray[j].wallpaperPlugin = 'org.kde.image'
     desktopsArray[j].currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
     desktopsArray[j].writeConfig("Image", "file:///usr/share/wallpapers/core-16.09/netrunner-core.png")
+    desktopsArray[j].writeConfig("FillMode", "2")
 
     desktopsArray[j].currentConfigGroup = new Array("General");
     desktopsArray[j].writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,4,desktop:/secure-remote.desktop,0,3")
