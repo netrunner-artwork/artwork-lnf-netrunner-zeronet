cc1c218efd4f579fff9dda48d3050da5f9aeaaf0
Rohan Garg <rohan@garg.io>
2017-06-27 17:06:15 +0200

    Update for Netrunner 17.06

--- usr/share/plasma/look-and-feel/org.kde.netrunner-core.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-core.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -2,11 +2,11 @@ applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
 applet.writeConfig("Image", "file:///usr/share/wallpapers/netrunner-core-17.01/netrunner-core.png")
 applet.writeConfig("FillMode", "2")
-applet.currentConfigGroup = new Array("General")
+applet.currentConfigGroup = new Array("General");
 applet.writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,4,desktop:/secure-remote.desktop,0,3")
-applet.writeConfig("pressToMove",true)
-applet.writeConfig("showToolbox",false)
-applet.writeConfig("popups",false)
-applet.writeConfig("selectionMarkers",false)
-applet.writeConfig("sortMode","-1")
+applet.writeConfig("pressToMove",true);
+applet.writeConfig("showToolbox",false);
+applet.writeConfig("popups",false);
+applet.writeConfig("selectionMarkers",false);
+applet.writeConfig("sortMode","-1");
 applet.reloadConfig()
