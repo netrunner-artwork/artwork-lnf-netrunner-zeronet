065926167768255a270dc1080489b39aef264c15
Rohan Garg <rohan@garg.io>
2017-06-27 17:02:44 +0200

    Update for Netrunner 17.06

--- usr/share/plasma/look-and-feel/org.kde.netrunner-core.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-core.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
@@ -3,7 +3,8 @@ applet.writeConfig("global", "Alt+F1")
 
 applet.currentConfigGroup = ["General"]
 applet.writeConfig("icon", "file:///usr/share/icons/hicolor/scalable/apps/homerun.svg")
-applet.writeConfig("favorites", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "org.kde.konsole.desktop", "synaptic.desktop"])
-applet.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop")
+applet.writeConfig("favorites", ["org.kde.ksysguard.desktop", "systemsettings.desktop", "synaptic.desktop"])
 applet.writeConfig("alphaSort", true)
+applet.writeConfig("hiddenApplications", "org.kde.ksshaskpass.desktop,libreoffice-draw.desktop,libreoffice-base.desktop,libreoffice-math.desktop,lximage-qt-screenshot.desktop,audacious-qt.desktop,mpv.desktop")
+
 applet.reloadConfig();
