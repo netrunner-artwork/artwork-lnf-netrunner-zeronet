33759c381bf66edd3a0dc8aa4db49d3504a9d708
llelectronics <leszek.lesner@web.de>
2017-12-12 17:49:22 +0100

    Set default location of zeronet

--- usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -3,5 +3,7 @@ loadTemplate("org.kde.plasma.desktop.defaultPanel")
 var desktopsArray = desktopsForActivity(currentActivity());
 
 for (var i = 0; i < desktopsArray.length; i++) {
-	desktopsArray[i].addWidget("org.kde.zerocontrol")
+	var zerowidget = desktopsArray[i].addWidget("org.kde.zerocontrol")
+	zerowidget.currentConfigGroup = ["Configuration"]["General"]
+	zerowidget.writeConfig("zeronetLocation[$e]", "/opt/zeronet/ZeroNet-master")
 }
