d24a64c847f34a8374322c3b66e554f439811de0
llelectronics <leszek.lesner@web.de>
2017-12-12 18:07:33 +0100

    Fix last commits scripting syntax

--- usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -4,6 +4,6 @@ var desktopsArray = desktopsForActivity(currentActivity());
 
 for (var i = 0; i < desktopsArray.length; i++) {
 	var zerowidget = desktopsArray[i].addWidget("org.kde.zerocontrol")
-	zerowidget.currentConfigGroup = ["Configuration"]["General"]
-	zerowidget.writeConfig("zeronetLocation[$e]", "/opt/zeronet/ZeroNet-master")
+	zerowidget.currentConfigGroup = '["Configuration"]["General"]'
+	zerowidget.writeConfig("zeronetLocation", "/opt/zeronet/ZeroNet-master")
 }
