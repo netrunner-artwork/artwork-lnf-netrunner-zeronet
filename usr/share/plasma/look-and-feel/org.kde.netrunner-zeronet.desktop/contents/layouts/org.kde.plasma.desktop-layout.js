5b18211f68f4a2f39ceffcd16d9581cf602a1189
llelectronics <leszek.lesner@web.de>
2017-12-14 14:24:09 +0100

    Set geometry for zerowidget

--- usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -4,6 +4,8 @@ var desktopsArray = desktopsForActivity(currentActivity());
 
 for (var i = 0; i < desktopsArray.length; i++) {
 	var zerowidget = desktopsArray[i].addWidget("org.kde.zerocontrol")
+	zerowidget.geometry.x = 200
+	zerowidget.geometry.y = 200
 	zerowidget.currentConfigGroup = '["Configuration"]["General"]'
 	zerowidget.writeConfig("zeronetLocation", "/opt/zeronet/ZeroNet-master")
 }
