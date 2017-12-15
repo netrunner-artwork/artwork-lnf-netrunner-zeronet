a64845f5197506a1dcf1c35cffa7f0d5ece56910
llelectronics <leszek.lesner@web.de>
2017-12-15 10:22:43 +0100

    Fixed positioning zeronet widget

--- usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -3,9 +3,7 @@ loadTemplate("org.kde.plasma.desktop.defaultPanel")
 var desktopsArray = desktopsForActivity(currentActivity());
 
 for (var i = 0; i < desktopsArray.length; i++) {
-	var zerowidget = desktopsArray[i].addWidget("org.kde.zerocontrol")
-	zerowidget.geometry.x = 200
-	zerowidget.geometry.y = 200
+	var zerowidget = desktopsArray[i].addWidget("org.kde.zerocontrol", 200,200,256,192)
 	zerowidget.currentConfigGroup = '["Configuration"]["General"]'
 	zerowidget.writeConfig("zeronetLocation", "/opt/zeronet/ZeroNet-master")
 }
