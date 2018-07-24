7cd8c35809e444c4e561abbe7205043d300c2819
Leszek Lesner <leszek.lesner@web.de>
2018-07-24 16:41:09 +0200

    New core wallpaper as default

--- usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,6 +1,6 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
-applet.writeConfig("Image", "file:///usr/share/wallpapers/netrunner-desktop-18.03/idolon-1920x1080.jpg")
+applet.writeConfig("Image", "file:///usr/share/wallpapers/netrunner-core-18.08/netrunner-core.png")
 applet.writeConfig("FillMode", "2")
 applet.currentConfigGroup = new Array("General");
 applet.writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,4,desktop:/secure-remote.desktop,0,3")
