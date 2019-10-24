f5decec63257c935343d1978fdc6ffe3d829dc3a
Scarlett Moore <sgmoore@kde.org>
2019-10-24 16:47:40 -0700

    Adjust desktop icons for readme removal.

--- usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-zeronet.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -2,7 +2,7 @@ applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
 applet.writeConfig("Image", "file:///usr/share/wallpapers/netrunner-zeronet/wallpaper-zeronet.png")
 applet.currentConfigGroup = new Array("General");
-applet.writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,4,desktop:/secure-remote.desktop,0,3")
+applet.writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/calamares.desktop,0,3,desktop:/secure-remote.desktop,0,2")
 applet.writeConfig("pressToMove",true);
 applet.writeConfig("showToolbox",false);
 applet.writeConfig("popups",false);
